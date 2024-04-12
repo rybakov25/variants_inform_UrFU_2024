546abc4ed441d9b0e8fad9419a81010d6978e5b6
Pavel Rybakov <rybakovpavel9@gmail.com>
2024-04-12 22:43:26 +0500

    Added examples 3-5 var1 for Pascal

---
+++ example 4/Pascal/variant 1/var1.pas
@@ -0,0 +1,9 @@
+﻿var
+    a, i, j, k: integer;
+
+begin
+    Write('Введите начальное значение диапозона: ');
+    Readln(a);
+    for i := a to 10 do
+        Writeln(i, ' ^ 2 = ', i*i);
+end.
---
+++ example 5/Pascal/variant 1/var1.pas
@@ -0,0 +1,39 @@
+﻿var
+    a: array [1..1000] of integer;
+    chislo, k: integer;
+    bin, sept: string;
+    i, n: longint;
+
+begin
+    n := 1;
+    Write('Введите число: '); Readln(chislo);
+    
+    bin := IntToStr(chislo);
+    
+    for i := 1 to 1000 do
+    begin
+        if (chislo mod 10) = 0 then
+        begin
+            a[i] := 0;
+            chislo := chislo div 10;
+        end
+        else
+        begin
+            a[i] := 1;
+            chislo := chislo div 10;
+        end;
+    end;
+    for i := 1 to 1000 do
+    begin
+        k := k + a[i] * n;
+        n := n * 2;
+    end;
+    
+    for i := 1 to length(bin) do n := 2 * n + (ord(bin[i]) - ord('0'));
+    repeat
+    sept := char(n mod 7 + ord('0')) + sept;
+    n := n div 7
+    until n = 0;
+    writeln('Семеричное число: ', sept);
+    Write('Десятичное число: ', k);
+end.
