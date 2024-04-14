3d6081514d93421658fb1cc5f12ef023f2ac7406
Pavel Rybakov <rybakovpavel9@gmail.com>
2024-04-14 18:05:08 +0500

    Added variants 1 and 6 for Pascal

---
+++ example 2/Pascal/variant 6/var6.pas
@@ -0,0 +1,48 @@
+﻿var
+  resistences1group: array[1..3] of real; {первая группа резисторов}
+  resistences2group: array[1..2] of real; {вторая группа резисторов}
+  resistences3group: array[1..2] of real; {третья группа резисторов}
+  i: integer;
+  CircuitResistance: real;
+
+{функция вычисления сопротивления последовательно соединенных резисторов}
+function SequentialResistance(const r: array of real): real;
+var res: real; i: integer;
+begin
+  res := 0;
+  for i := 1 to High(r) do
+    res := res + r[i];
+  SequentialResistance := res;
+end;
+
+{функция вычисления сопротивления параллельно соединенных резисторов}
+function ParallelResistance(const r: array of real): real;
+var res: real; i: integer;
+begin
+  res := 0;
+  for i := 1 to High(r) do
+    res := res + (1/r[i]);
+  ParallelResistance := res;
+end;
+
+begin
+  for i := 1 to High(resistences1group) do
+  begin
+    Write('Введите сопротивление резистора ', i, ': ');
+    Readln(resistences1group[i]);
+  end;
+  for i := 1 to High(resistences2group) do
+  begin
+    Write('Введите сопротивление резистора ', i+3, ': ');
+    Readln(resistences2group[i]);
+  end;
+  for i := 1 to High(resistences3group) do
+  begin
+    Write('Введите сопротивление резистора ', i+5, ': ');
+    Readln(resistences3group[i]);
+  end;
+  CircuitResistance := ParallelResistance(resistences1group)
+  + SequentialResistance(resistences2group)
+  + ParallelResistance(resistences3group);
+  Write('Сопротивление цепи равно: ', CircuitResistance:0:3, ' Ом');
+end.
---
+++ example 5/Pascal/variant 6/var6.pas
@@ -0,0 +1,33 @@
+﻿var
+  s: string; {исходное число для перевода}
+  i,b: integer; {b - основание системы счисления исходного числа}
+  d,k: double;
+  f,e: boolean;
+ 
+begin
+  Write('Ввведите основание системы: ');
+  ReadLn(b);
+  e:=b<=1;
+  if e then
+    WriteLn('Ошибка: основание системы должно быть не менее 2')
+  else begin
+    Write('Введите число в системе с основанием ',b,': ');
+    ReadLn(s);
+    f:=false;
+    k:=1;
+    for i:=1 to Length(s) do if Ord(s[i])<48+b then begin
+      if f then k:=k/b;
+      case s[i] of
+        '0'..'9': d:=d*b+Ord(s[i])-48;
+        '.',',': if f then e:=true else f:=true;
+        else f:=true
+      end;
+      if e then break
+    end
+    else e:=true;
+    if e then
+      WriteLn('Ошибка в записи числа')
+    else
+      WriteLn('Значение в десятичной системе: ',d*k)
+  end;
+end.
