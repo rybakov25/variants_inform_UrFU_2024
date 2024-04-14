8ea234aae6a58f0e270b7e60cd58515f1c95c675
Pavel Rybakov <rybakovpavel9@gmail.com>
2024-04-15 01:04:17 +0500

    Added variant 13, example 1 and 5 on C#

---
+++ example 1/C#/variant 13/var13.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double S, V, R, r, d;
+            Console.Write("Введите радиус сферы: ");
+            R = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Введите толщину стенки: ");
+            d = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r = R - d;
+            V = (4 / 3) * Math.PI * (R - r);
+            S = 4 * Math.PI * Math.Pow(R, 2) * d;
+            Console.WriteLine($"Объем материала сферы: V = {V}");
+            Console.WriteLine($"Площадь поверхности сферы: S = {S}");
+        }
+    }
+}
---
+++ example 5/C#/variant 13/var13.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n; // исходное число
+            string s8, s16; // проебразованные числа
+            Console.Write("Введите двоичное число: ");
+            n = Convert.ToInt32(Console.ReadLine(), 2);
+            s8 = Convert.ToString(n, 8);
+            s16 = Convert.ToString(n, 16);
+            Console.WriteLine($"Base 8: {s8}\tBase 16: {s16}");
+        }
+    }
+}
